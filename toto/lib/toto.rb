9dd77f378ad2f3566788ae6ebd4dc8ba8dfed308
Tom-Eric <ik@tom-eric.info>
2010-04-08 20:36:41 +0200

    We use articles/title-slug.txt

--- toto/lib/toto.rb
+++ toto/lib/toto.rb
@@ -84,7 +84,7 @@ module Toto
     end
 
     def article route
-      Article.new("#{Paths[:articles]}/#{route.join('-')}.#{self[:ext]}", @config).load
+      Article.new("#{Paths[:articles]}/#{route.last}.#{self[:ext]}", @config).load
     end
 
     def /
